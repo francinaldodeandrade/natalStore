3171d2cc6f1c729d0d0b18836f4a287f736dd740
Naldo de Andrade <naldo.dev@outlook.com>
2024-07-08 22:19:07 -0300

    subindo page users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Home from "./componentes/pages/Home";
-
+//import Home from "./componentes/pages/Home";
+import Users from "./componentes/pages/Users";
 import "./styles/normalize.css"
 import "./styles/fontawesome.min.css"
 import "./styles/main.css"
 
 function App() {
   return (
-    <Home/>
+    <Users/>
   );
 }
 
