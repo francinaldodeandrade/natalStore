102a93d97e6d08a1d4461b65b1b2dea168082fc4
Naldo de Andrade <naldo.dev@outlook.com>
2024-07-08 21:51:20 -0300

    conversão html em jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Home from "./componentes/pages/Home";
+
+import "./styles/normalize.css"
+import "./styles/fontawesome.min.css"
+import "./styles/main.css"
+
 function App() {
   return (
-    <div>projetos</div>
+    <Home/>
   );
 }
 
